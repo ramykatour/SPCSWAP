bded50fca247f88178dbe09c48f7b1db81a5069b
Ramy Katour <ramymouner@hotmail.com>
2025-02-06 20:30:09 +0200

    update menu

--- src/components/Menu/config/config.ts
+++ src/components/Menu/config/config.ts
@@ -1,3 +1,41 @@
+import {
+  MenuItemsType,
+  DropdownMenuItemType,
+  SwapIcon,
+  SwapFillIcon,
+  EarnFillIcon,
+  EarnIcon,
+  TrophyIcon,
+  TrophyFillIcon,
+  NftIcon,
+  NftFillIcon,
+  MoreIcon,
+} from '@pancakeswap/uikit'
+import { ContextApi } from '@pancakeswap/localization'
+import { nftsBaseUrl } from 'views/Nft/market/constants'
+import { perpLangMap } from 'utils/getPerpetualLanguageCode'
+import { perpTheme } from 'utils/getPerpetualTheme'
+import { DropdownMenuItems } from '@pancakeswap/uikit/src/components/DropdownMenu/types'
+import { SUPPORT_ONLY_BSC } from 'config/constants/supportChains'
+
+export type ConfigMenuDropDownItemsType = DropdownMenuItems & { hideSubNav?: boolean }
+export type ConfigMenuItemsType = Omit<MenuItemsType, 'items'> & { hideSubNav?: boolean; image?: string } & {
+  items?: ConfigMenuDropDownItemsType[]
+}
+
+const addMenuItemSupported = (item, chainId) => {
+  if (!chainId || !item.supportChainIds) {
+    return item
+  }
+  if (item.supportChainIds?.includes(chainId)) {
+    return item
+  }
+  return {
+    ...item,
+    disabled: true,
+  }
+}
+
 const config: (
   t: ContextApi['t'],
   isDark: boolean,
